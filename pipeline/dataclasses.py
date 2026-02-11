4bb7bce266226929d17b3d38b64d4a329a86ef79
Ayushman-Choudhuri <ayushc205@gmail.com>
2026-02-11 18:22:20 +0100

    refac: moved dataclasses to a separate file

---
+++ pipeline/dataclasses.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import (
+    Any, 
+    Dict,
+)
+
+@dataclass
+class StreamMessage:
+    topic: str
+    msg: Any
+    timestamp: float
+
+@dataclass
+class SyncGroup:
+    timestamp: float
+    lidar: Any
+    camera: Any
+    transforms: Dict[str, Any]
